4c85385a6f899025b14bb98ab2be89f743e84ba5
Justin SB <justin@fathomdb.com>
2014-04-29 22:40:19 -0700

    Try configuring load balancer

--- jujuxaas/stub.py
+++ jujuxaas/stub.py
@@ -68,7 +68,54 @@ class Stub(object):
   def on_stop(self):
     return None
 
+  def _run_loadbalancer_hook(self, action):
+    logger.info("Running load-balancer hook %s", action)
+
+    host = Juju.private_address()
+    private_port = self.config['private-port']
+    public_port = self.config['public-port']
+
+    if private_port == 0:
+      logger.info("Private port is 0; won't configure load balancer")
+
+    if public_port == 0:
+      logger.info("Public port is 0; won't configure load balancer")
+
+    unit_id = Juju.unit_name()
+
+    relation = Relation.default()
+    relation_id = relation.relation_id
+
+    servers = []
+    servers.append(['s_1', host, private_port])
+
+    settings = {}
+    settings['service_name'] = unit_id + '_' + relation_id
+    settings['service_options'] = [ 'mode http', 'balance lastconn' ]
+    settings['servers'] = servers
+
+    new_properties = {}
+    new_properties['services'] = yaml.dump(settings)
+
+    logger.info("Setting relation properties to: %s", new_properties)
+    relation.set_properties(relation_properties.get('Properties', {}))
+
+#     relation-set "services=
+#     - { service_name: my_web_app,
+#         service_options: [mode http, balance leastconn],
+#         servers: [[my_web_app_1, $host, $port, option httpchk GET / HTTP/1.0],
+#                   [... optionally more servers here ...]]}
+#     - { ... optionally more services here ... }
+#     "
+
+    if new_properties:
+        logger.info("Setting relation properties to: %s", new_properties)
+        relation.set_properties(new_properties)
+
   def run_relation_hook(self, relation_name, action):
+    if relation_name == 'website':
+      return self._run_website_hook(action)
+
     # TODO: Only on certain actions?
     logger.info("Running relation hook %s %s", relation_name, action)
 
